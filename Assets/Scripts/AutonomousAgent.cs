ac7607199a2ffdfb4ca48e13cce86f77af5a3f21
logan-felix <158220968+logan-felix@users.noreply.github.com>
2025-01-24 23:04:16 -0700

    Autonomous Agents
    Seek, Flee, and Wander behaviors added

--- Assets/Scripts/AutonomousAgent.cs
+++ Assets/Scripts/AutonomousAgent.cs
@@ -2,16 +2,140 @@ using UnityEngine;
 
 public class AutonomousAgent : AIAgent
 {
-    public Perception perception;
+    [SerializeField] AutonomousAgentData data;
+
+    [Header("Perception")]
+    public Perception seekPerception;
+    public Perception fleePerception;
+    public Perception flockPerception;
+
+    float angle;
 
     private void Update()
     {
-        Debug.DrawRay(transform.position, transform.forward * perception.maxDistance, Color.yellow);
+        //movement.ApplyForce(Vector3.forward * 10);
+        float size = 20;
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-size, -size, -size), new Vector3(size, size, size));
 
-        var gameObjects = perception.GetGameObjects();
-        foreach (var go in gameObjects)
+        //Debug.DrawRay(transform.position, transform.forward * seekPerception.maxDistance, Color.yellow);
+
+        // SEEK
+        if (seekPerception != null)
         {
-            Debug.DrawLine(transform.position, go.transform.position, Color.red);
+            Debug.DrawRay(transform.position, transform.forward * seekPerception.maxDistance, Color.yellow);
+
+            var gameObjects = seekPerception.GetGameObjects();
+            if (gameObjects.Length > 0)
+            {
+                Vector3 force = Seek(gameObjects[0]);
+                movement.ApplyForce(force);
+            }
         }
+
+        // FLEE
+        if (fleePerception != null)
+        {
+            Debug.DrawRay(transform.position, transform.forward * fleePerception.maxDistance, Color.red);
+
+            var gameObjects = fleePerception.GetGameObjects();
+            if (gameObjects.Length > 0)
+            {
+                Vector3 force = Flee(gameObjects[0]);
+                movement.ApplyForce(force);
+            }
+        }
+
+        // FLOCK
+        if (flockPerception != null)
+        {
+            var gameObjects = flockPerception.GetGameObjects();
+            if (gameObjects.Length > 0)
+            {
+                movement.ApplyForce(Cohesion(gameObjects) * data.cohesionWeight);
+                movement.ApplyForce(Separation(gameObjects, data.separationRadius) * data.separationWeight);
+                movement.ApplyForce(Alignment(gameObjects) * data.alignmentWeight);
+            }
+        }
+
+        // WANDER - if not moving (seek/flee)
+        if (movement.Acceleration.sqrMagnitude == 0)
+        {
+            Vector3 force = Wander();
+            movement.ApplyForce(force);
+        }
+
+        Vector3 acceleration = movement.Acceleration;
+        acceleration.y = 0;
+        movement.Acceleration = acceleration;
+
+        if (movement.Direction.sqrMagnitude != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(movement.Direction);
+        }
+        //foreach (var go in gameObjects)
+        //{
+        //    Debug.DrawLine(transform.position, go.transform.position, Color.red);
+        //}
+    }
+
+    private Vector3 Cohesion(GameObject[] neighbors)
+    {
+        Vector3 positions = Vector3.zero;
+        foreach (var neighbor in neighbors)
+        {
+            positions += neighbor.transform.position;
+        }
+
+        Vector3 center = positions / neighbors.Length;
+        Vector3 direction = center - transform.position;
+
+        Vector3 force = GetSteeringForce(direction);
+
+        return force;
+    }
+
+    private Vector3 Separation(GameObject[] neighbors, float radius)
+    {
+        return Vector3.zero;
+    }
+
+    private Vector3 Alignment(GameObject[] neighbors)
+    {
+        return Vector3.zero;
+    }
+
+    private Vector3 Seek(GameObject go)
+    {
+        Vector3 direction = go.transform.position - transform.position;
+        Vector3 force = GetSteeringForce(direction);
+
+        return force;
+    }
+
+    private Vector3 Flee(GameObject go)
+    {
+        Vector3 direction = transform.position - go.transform.position;
+        Vector3 force = GetSteeringForce(direction);
+
+        return force;
+    }
+
+    private Vector3 Wander()
+    {
+        angle += Random.Range(-data.displacement, data.displacement);
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        Vector3 point = rotation * (Vector3.forward * data.radius);
+        Vector3 forward = transform.forward + movement.Direction;
+        Vector3 force = GetSteeringForce(forward + point);
+        return force;
+    }
+
+    private Vector3 GetSteeringForce(Vector3 direction)
+    {
+        Vector3 desired = direction.normalized * movement.data.maxSpeed;
+        Vector3 steer = desired - movement.Velocity;
+        Vector3 force = Vector3.ClampMagnitude(steer, movement.data.maxForce);
+
+        return force;
     }
 }
---
+++ Assets/Scripts/AutonomousAgentData.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AutonomousAgentData", menuName = "Data/AutonomousAgentData")]
+public class AutonomousAgentData : ScriptableObject
+{
+    [Range(0, 10)] public float displacement;
+    [Range(0, 10)] public float distance;
+    [Range(0, 10)] public float radius;
+
+    [Range(0, 5)] public float cohesionWeight;
+    [Range(0, 5)] public float separationWeight;
+    [Range(0, 5)] public float separationRadius;
+    [Range(0, 5)] public float alignmentWeight;
+}
